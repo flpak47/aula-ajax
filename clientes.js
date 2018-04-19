bb88c7a00a79595aefe1427e274c4dc0979a9a57
fabio <flp_padilha@hotmail.com>
2018-04-18 21:45:52 -0300

    add datepicker

--- clientes.js
+++ clientes.js
@@ -29,8 +29,65 @@ $(function(){
 
                 $("tbody").append(tr);
 
-            });
+            }); //fim do foreach
 
-        });           
+        }); //fim do getJSON
+        
+        $("fieldset button").click(function(){
+           
+
+            var nome = $("input[name=nome]").val();
+            var email = $("input[name=email]").val();
+            var cep = $("input[name=cep]").val();
+            var dataNasc = $("input[name=dataNasc]").val();
+            
+            var tr = '<tr>'
+                + '<td>'+ nome +'</td>'
+                + '<td>'+ email +'</td>'
+                + '<td>'+ cep +'</td>'
+                + '<td>'+ dataNasc +'</td>'
+                + '</tr>';
+
+            var tr2 = $(tr).hide().fadeIn(2000);    
+
+            $("tbody").append(tr2);
+
+        });
+
+        $("input[name=cep]").keydown(function(ev){
+
+            if (ev.keyCode < 48 || ev.keyCode > 57)
+            {
+            return false;
+            }    
+        });
+
+        $("input[name=dataNasc]").datepicker({
+            dateFormat: "dd/mm/yy"
+        });
+
+        $("input[name=email]").blur(function(){
+
+            var valor = $(this).val();
+
+            var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+            regex.test(valor);
+
+            if(regex.test(valor) == false)
+            {
+                alert("Email Inválido");
+            }
+
+        });
+
+
+    
+          
+
+
             
     });
+
+
+ 
